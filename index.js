1a577a88d5f0221cd3b16a36f6e652150435bd73
amaan-bhati <amaanbhati49@gmail.com>
2022-03-01 16:33:00 +0530

    image and function setup

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let pieces = [];
+let cols = 4;
+let rows = 4;
+let w,h;
+
+function preLoad(){
+    source = loadImage("fc-bayern.png");
+}
+function setup(){
+    createCanvas(400,400);
+    for (let i = 0 ; i<cols;i++){
+        for (let j = 0 ; j<rows;j++){
+
+    }
+}
